020b1e458275dd2a568cbc25f1b3af10dc17a335
Umut Hope YILDIRIM <umut475@gmail.com>
2022-12-21 19:40:45 +0300

    YEAHHH no more lorem ipsum

--- src/components/Landing/OurMission.jsx
+++ src/components/Landing/OurMission.jsx
@@ -1,6 +1,6 @@
 import { Hero } from "react-daisyui";
 
-function OurMission({ img, imgAlt, title, description }) {
+function OurMission({ img, imgAlt, money ,title, description }) {
   return (
     <Hero className="rounded-lg px-10 py-10">
         <Hero.Content className="flex flex-col md:flex-row rounded-lg">
@@ -11,6 +11,7 @@ function OurMission({ img, imgAlt, title, description }) {
             />
             <div>
                 <h1 className="text-5xl font-bold">{title}</h1>
+                <p className="pt-6">Average Salaries in 2022: <p className="font-bold">{money}</p></p>
                 <p className="py-6">{description}</p>
             </div>
         </Hero.Content>
