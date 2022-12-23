d190d02292b8e882de80a9457d6e3d8be15f76d5
Umut Hope YILDIRIM <umut475@gmail.com>
2022-12-23 20:29:22 +0300

    Created 404 page

---
+++ src/components/Errors/ErrorsHero.jsx
@@ -0,0 +1,22 @@
+import { Hero, Button } from "react-daisyui"
+
+function ErrorsHero({ errorCode, errorTitle, errorDescription, errorImage, errorImageAlt }) {
+  return(
+    <Hero>
+      <Hero.Overlay />
+      <Hero.Content className="text-center md:flex md:shrink-0">
+        <div className="max-w-md">
+            <img className="py-3" src={errorImage} alt={errorImageAlt} />
+            <h1 className="font-bold">{errorCode}</h1>
+            <h1 className="text-5xl font-bold">{errorTitle}</h1>
+            <p className="py-6">
+                {errorDescription}
+            </p>
+            <Button href="/" color="accent">Go to main page</Button>
+        </div>
+      </Hero.Content>
+    </Hero>
+  )
+}
+
+export default ErrorsHero;
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import './assets/css/index.css'
 import Landing from './views/Landing'
 import Courses from './views/Courses'
 
+/* Error Pages */
+import NotFound from './views/errors/NotFound'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -15,6 +18,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         {/* Landing Pages */}
         <Route path="/" element={<Landing />} />
         <Route path="/courses" element={<Courses />} />
+
+        {/* Error Pages */}
+        <Route path='*' element={<NotFound />}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
