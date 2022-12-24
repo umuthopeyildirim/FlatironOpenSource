49e110f5a6c60276d39489d6b02ea11bd4a4a2fd
ian gottheim <iangottheim@gmail.com>
2022-12-24 07:17:43 -0500

    added core bar and core footer to course page

--- src/views/Course.jsx
+++ src/views/Course.jsx
@@ -1,11 +1,15 @@
 import CoursesHero from "../components/Courses/CoursesHero";
 import CourseTextArea from "../components/Course/CourseTextArea";
+import CoreBar from "../components/CoreBar";
+import CoreFooter from "../components/CoreFooter";
 
 function Course() {
   return (
     <div>
+   <CoreBar/>
    <CoursesHero/>
    <CourseTextArea/>
+   <CoreFooter/>
     </div>
   )
 }
