1badafba618f4aabf048c5f29f2ffba9352d030a
Umut Hope YILDIRIM <umut475@gmail.com>
2022-12-24 20:09:33 +0300

    Dont work

--- flatironopensource/src/index.js
+++ flatironopensource/src/index.js
@@ -1,22 +1,27 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    const key = url.pathname.slice(1);
-    switch (request.method) {
-      case "GET":
-        const object = await env.bucket.get(key);
-        if (object === null) {
-          return new Response("Object Not Found", { status: 404 });
-        }
-        const headers = new Headers();
-        object.writeHttpMetadata(headers);
-        headers.set("etag", object.httpEtag);
-        return new Response(object.body, { headers });
-      default:
-        return new Response("Method Not Allowed", {
-          status: 405,
-          headers: { Allow: "GET" },
-        });
-    }
-  },
-};
+    return await handleRequest(request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  }
+}
+
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
+
+  if (pathname.startsWith("/api")) {
+    return new Response(JSON.stringify({ pathname }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  if (pathname.startsWith("/status")) {
+    const httpStatusCode = Number(pathname.split("/")[2]);
+
+    return Number.isInteger(httpStatusCode)
+      ? fetch("https://http.cat/" + httpStatusCode)
+      : new Response("That's not a valid HTTP status code.");
+  }
+
+  return fetch("https://welcome.developers.workers.dev");
+}
