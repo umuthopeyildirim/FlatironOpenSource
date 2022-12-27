5b3cb0803a5e89e0efb42421b1b154e97324a11e
Umut Hope YILDIRIM <umut475@gmail.com>
2022-12-27 06:25:13 +0300

    For the future

--- src/components/Course/CourseTextArea.jsx
+++ src/components/Course/CourseTextArea.jsx
@@ -5,16 +5,19 @@ function CourseTextArea({course, phase}) {
   return(
     <div className="flex md:flex-row flex-col mx-5">
       <ButtonGroup className="md:w-60 w-full" vertical>
+        {/* Lessons go hereeeee */}
         {course.phases.map((phase) => (
           <Button key={phase.name} href={phase.link}>{phase.name}</Button>
         ))}
       </ButtonGroup>
       <div className="flex flex-col w-full md:ml-5 mt-2.5">
         <div className="flex flex-row justify-between">
+          {/* Lesson name goes hereeee */}
           <h1 className="text-3xl font-bold">{course.name}: {phase.name}</h1>
           <Button startIcon={BsGithub} href={course.link} size="md">Fork Github</Button>
         </div>
         <div className="mt-2">
+          {/* Markdown content hereeee */}
           {course.description}
         </div>
       </div>
