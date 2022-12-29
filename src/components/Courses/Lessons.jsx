2d3dc0ace96b006209b13ee1e3b9fc24f73c893c
Umut Hope YILDIRIM <umut475@gmail.com>
2022-12-30 01:38:08 +0300

    Fixed Courses mobile

--- src/components/Courses/Lessons.jsx
+++ src/components/Courses/Lessons.jsx
@@ -2,12 +2,12 @@ import { Hero, Button } from "react-daisyui";
 
 function Lessons({ img, imgAlt ,title, description, link }) {
     return (
-        <Hero className="rounded-lg px-10 py-10">
+        <Hero>
             <Hero.Content className="flex flex-col md:flex-row rounded-lg">
                 <img
                     alt={imgAlt}
                     src={img}
-                    className="max-w-sm rounded-lg"
+                    className="md:max-w-md rounded-lg"
                 />
                 <div>
                     <h1 className="text-5xl font-bold">{title}</h1>
