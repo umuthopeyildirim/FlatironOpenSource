f4a3c12021856c145cf89a0d13180eaaab6eb1b6
Umut Hope YILDIRIM <umut475@gmail.com>
2023-01-02 19:41:26 +0300

    Changed robots.txt and Salary Component

--- src/components/Landing/About.jsx
+++ src/components/Landing/About.jsx
@@ -5,25 +5,18 @@ function About(){
         <Hero>
             <Hero.Content className="rounded-lg px-10 py-10">
                 <div className="flex flex-col text-center">
-                    <img
-                        alt="Why do i need Flatiron School?"
-                        src="/img/about.webp"
-                        className="rounded-lg"
-                    />
-                    <div>
-                        <h1 className="text-5xl font-bold">
-                            About Flatiron School
-                        </h1>
-                        <p className="py-6">
-                            Flatiron's courses provide an immersive, outcomes-driven curriculum for students looking
-                            to launch a career in Software Engineering, Cybersecurity, Data Science, or Product
-                            Design. Courses are offered both online and in-person on campuses across the country.
-                        </p>
-                        <p>
-                            Flatiron School is frequently ranked as a top coding bootcamp by industry publications.
-                            In addition to student programs, Flatiron also offers targeted education solutions for organizations.
-                        </p>
-                    </div>
+                    <h1 className="text-5xl font-bold">
+                        About Flatiron School
+                    </h1>
+                    <p className="py-6">
+                        Flatiron's courses provide an immersive, outcomes-driven curriculum for students looking
+                        to launch a career in Software Engineering, Cybersecurity, Data Science, or Product
+                        Design. Courses are offered both online and in-person on campuses across the country.
+                    </p>
+                    <p>
+                        Flatiron School is frequently ranked as a top coding bootcamp by industry publications.
+                        In addition to student programs, Flatiron also offers targeted education solutions for organizations.
+                    </p>
                 </div>
             </Hero.Content>
         </Hero>
--- src/components/Landing/Salaries.jsx
+++ src/components/Landing/Salaries.jsx
@@ -2,6 +2,10 @@ import { Hero } from "react-daisyui";
 
 function Salaries({ img, imgAlt, money ,title, description }) {
   return (
+    <>                
+    <h1 className="text-5xl font-bold px-6">
+      Salary Information for Careers in Tech
+    </h1>
     <Hero>
         <Hero.Content className="rounded-lg px-10 py-10">
           <div className="flex flex-col md:flex-row	items-center">
@@ -18,6 +22,7 @@ function Salaries({ img, imgAlt, money ,title, description }) {
           </div>
         </Hero.Content>
     </Hero>
+    </>
   );
 }
 
