3d263a5bfc6374815d3d072d65beff321516ebab
Umut Hope YILDIRIM <umut475@gmail.com>
2023-01-02 16:54:26 +0300

    Added Google Analytics

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import ViteRadar from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    ViteRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-QYFSS4RFJQ',
+      },
+      gtm: {
+        id: 'GTM-TTWKD6W',
+      },
+    })
   ],
 })
