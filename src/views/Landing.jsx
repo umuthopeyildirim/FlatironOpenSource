71dc918aab84beb4dedacb002e6902be61601c23
ian gottheim <iangottheim@gmail.com>
2023-01-18 09:22:34 -0500

    fix alert for GDPR

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -14,18 +14,17 @@ function Landing() {
 
 //Google Analytics Alert Impleenting local storage
 //if the alert is saved in local storage, then consent for the current user already occurred so the alert won't pop up again
-const [alertOn, setAlertOn] = useState(true)
+ 
 
 function setAlerts(){
-  alert("Please know that we use Google Analytics and cookies to track user traffic for this site. By clicking ok, you consent to allow this to occur.")
-  setAlertOn(!alertOn)
-  localStorage.setItem(alertOn, false)
+  alert("Please know that we use Google Analytics and cookies to track user traffic for this site. By clicking ok, you consent to flatironopensource.com's Cookie and Privacy policies.")
+  localStorage.setItem("alertOn", false)
 }
 
 
   return (
     <div>
-     {!localStorage.getItem(alertOn)? setAlerts(): null}
+     {!localStorage.getItem("alertOn")? setAlerts(): null}
     
       <CoreBar />
      
