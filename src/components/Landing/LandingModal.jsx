bc376f05903e0114f0ea0cf223cb179244b9da87
ian gottheim <iangottheim@gmail.com>
2023-01-18 15:02:46 -0500

    fix gdpr

--- src/components/Landing/LandingModal.jsx
+++ src/components/Landing/LandingModal.jsx
@@ -4,8 +4,9 @@ import { useEffect, useState } from "react"
 //Google Analytics Pop Up
 
 function LandingModal() {
-
+  
 const [visible, setVisible] = useState(false)
+
 
 const toggleVisible = () => {
   setVisible(!visible)
