12cfce149ed59c5078b34616d1dbbfd2cb727b02
ian gottheim <iangottheim@gmail.com>
2023-01-18 14:46:42 -0500

    fix GDPR

--- src/components/Landing/LandingModal.jsx
+++ src/components/Landing/LandingModal.jsx
@@ -13,7 +13,7 @@ const toggleVisible = () => {
 
 return (
   <div className="font-sans">
-    <Button onClick={toggleVisible}>GDPR Security Disclaimer</Button>
+    <Button onClick={toggleVisible} >GDPR Security Disclaimer</Button>
     <Modal open={visible} className = "w-full text-sm text-white px-4 py-3 bg-gray-900 border border-gray-700 rounded-lg focus:outline-none focus:border-purple-400">
       <Modal.Header className="font-bold w-11/12 max-w-5xl bg-primary">
         GDPR Security Disclaimer
@@ -21,8 +21,8 @@ return (
 
       <Modal.Body className="font-bold w-11/12 max-w-5xl bg-primary">
       <p> Please note that we use Google Analytics and cookies to track user traffic for this site.</p>
-      <p>By clicking ok, you consent to flatironopensource.com's Cookies and Privacy policies. </p>
-      <p>Cookies and Privacy policies, as well as the site's Terms and Conditions, can be found in the footer below </p>
+      <p>By clicking "Confirm Consent", you consent to the storing of cookies on your device to enhance site navigation, analyze site usage, and assist in our marketing efforts. </p>
+      <p>Flatironopensource.com's Cookies and Privacy policies, as well as the site's Terms and Conditions, can be found in the footer below </p>
       </Modal.Body>
 
       <Modal.Actions>
