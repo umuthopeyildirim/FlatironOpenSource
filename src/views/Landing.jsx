2e5d6d0f06203008c7f92227525ababb862552d6
ian gottheim <iangottheim@gmail.com>
2023-01-18 09:19:39 -0500

    use local storage for GDPR alert

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -12,20 +12,23 @@ import { useState, useEffect } from "react";
 
 function Landing() {
 
-//Google Analytics Alert 
+//Google Analytics Alert Impleenting local storage
+//if the alert is saved in local storage, then consent for the current user already occurred so the alert won't pop up again
 const [alertOn, setAlertOn] = useState(true)
 
 function setAlerts(){
   alert("Please know that we use Google Analytics and cookies to track user traffic for this site. By clicking ok, you consent to allow this to occur.")
   setAlertOn(!alertOn)
+  localStorage.setItem(alertOn, false)
 }
 
 
   return (
     <div>
-     {alertOn? setAlerts(): null}
+     {!localStorage.getItem(alertOn)? setAlerts(): null}
     
       <CoreBar />
+     
       <LandingHero />
       <div id="lessons">
         <Lessons coursesData={coursesData} />
